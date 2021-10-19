5a888c64be2861463ee90fcc5ae0f7900fa98e02
Karan Parashar <whyredfire@gmail.com>
2021-10-19 19:11:45 +0530

    bahamut: drop wfdcommon

--- device.mk
+++ device.mk
@@ -199,8 +199,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
