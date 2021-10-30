fdd44cc6aae088bfdd40c27ac466d493b6fd7ecc
sjllls <2012ji123@gmail.com>
2021-10-30 19:33:47 +0800

    Revert "pdx201: Switch to QTI bt stack and enable ad2p offload"
    This reverts commit 5a66e0026fae30358d2cef4d5388b42d6ef0d07b.

--- device.mk
+++ device.mk
@@ -127,10 +127,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
 
-# QTI Bluetooth
-PRODUCT_PACKAGES += \
-    BluetoothQti
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
