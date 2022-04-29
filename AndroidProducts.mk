093ede6882ae0a9ad59eaecc46b6d48f5f29fde8
bobbyamarthaa <bobbyamarthaa@gmail.com>
2022-04-29 14:54:09 +0000

    BringUp BlissRoms

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_bahamut.mk
+    $(LOCAL_DIR)bliss_bahamut.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_bahamut-user \
-    evolution_bahamut-userdebug \
-    evolution_bahamut-eng
+    bliss_bahamut-user \
+    bliss_bahamut-userdebug \
+    bliss_bahamut-eng
